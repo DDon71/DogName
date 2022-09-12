7aa5ece2a65cfe76773520568dec40c12481d55f
DDon71 <theofficial1tap@gmail.com>
2022-09-12 13:02:13 -0400

    Dog name

---
+++ DogNameandAge/DogNameAge/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string firstName;
+int age;
+char middleInit;
+
+firstName = "Nicholas";
+age = 21;
+middleInit = 'J';
+
+bool isAlive = true;
+double weight = 155.4;
+decimal income = 30.5m;
+
+
+Console.WriteLine($"My name is {firstName} and I am {age} years old.");
+
+string dogName = "Lola";
+int dogAge = 13;
+
+Console.WriteLine($"My dog's name is {dogName} and She is {dogAge} years old.");
